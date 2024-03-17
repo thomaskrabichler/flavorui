9f2a7ab58bb53ac8326d37d06f777c432ae09c23
thomaskrabichler <thomaskrabichler@protonmail.com>
2024-03-17 16:42:36 +0100

    login action

--- src/lib/auth/actions.ts
+++ src/lib/auth/actions.ts
@@ -23,6 +23,61 @@ export interface ActionResponse<T> {
   formError?: string
 }
 
+export async function login(
+  _: any,
+  formData: FormData,
+): Promise<ActionResponse<LoginInput>> {
+  const obj = Object.fromEntries(formData.entries())
+
+  const parsed = loginSchema.safeParse(obj)
+  if (!parsed.success) {
+    const err = parsed.error.flatten()
+    return {
+      fieldError: {
+        email: err.fieldErrors.email?.[0],
+        password: err.fieldErrors.password?.[0],
+      },
+    }
+  }
+
+  const { email, password } = parsed.data
+
+  const existingUser = await db.query.users.findFirst({
+    where: (table, { eq }) => eq(table.email, email),
+  })
+
+  if (!existingUser) {
+    return {
+      formError: "Incorrect email or password",
+    }
+  }
+
+  if (!existingUser || !existingUser?.hashedPassword) {
+    return {
+      formError: "Incorrect email or password",
+    }
+  }
+
+  const validPassword = await new Argon2id().verify(
+    existingUser.hashedPassword,
+    password,
+  )
+  if (!validPassword) {
+    return {
+      formError: "Incorrect email or password",
+    }
+  }
+
+  const session = await lucia.createSession(existingUser.id, {})
+  const sessionCookie = lucia.createSessionCookie(session.id)
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  )
+  return redirect("/")
+}
+
 export async function signUp(
   _: any,
   formData: FormData,
@@ -64,7 +119,6 @@ export async function signUp(
   })
 
   //TODO: Maybe add email verification
-
   const session = await lucia.createSession(userId, {})
   const sessionCookie = lucia.createSessionCookie(session.id)
   cookies().set(

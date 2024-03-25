89624308752c53baf5a56024a0caeb4e453f4f47
thomaskrabichler <thomaskrabichler@protonmail.com>
2024-03-25 17:47:02 +0100

    removed getproductbyid route

--- src/server/api/routers/paddle/paddle.router.ts
+++ src/server/api/routers/paddle/paddle.router.ts
@@ -1,21 +1,17 @@
 import {
   createTRPCRouter,
-  protectedProcedure,
   publicProcedure,
 } from "../../trpc"
 import { z } from "zod"
 import { paddleService } from "./service/paddle.service"
-import { type GetProducts } from "./paddle.types"
+import { type GetProductsWithPrices } from "./paddle.types"
 
 export const paddleRouter = createTRPCRouter({
   getAllProducts: publicProcedure.query(
-    async ({ ctx }): Promise<GetProducts> => {
+    async ({ ctx }): Promise<GetProductsWithPrices> => {
       return paddleService.getProductsFromDb(ctx.db)
       // return paddleService.getProducts(ctx.paddle)
     },
   ),
 
-  getProductById: protectedProcedure
-    .input(z.object({ id: z.string() }))
-    .query(async ({ ctx, input }) => paddleService.getProducts(ctx.paddle)),
 })

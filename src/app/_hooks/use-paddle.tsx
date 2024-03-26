9556f4a7542f90131a328783036054803d2d2ae1
thomaskrabichler <thomaskrabichler@protonmail.com>
2024-03-26 13:57:34 +0100

    fixed checkout page not scrollable

--- src/app/_hooks/use-paddle.tsx
+++ src/app/_hooks/use-paddle.tsx
@@ -14,7 +14,6 @@ export default function usePaddle() {
     initializePaddle({
       environment: env.NEXT_PUBLIC_PADDLE_ENV,
       token: env.NEXT_PUBLIC_PADDLE_CLIENT_API_KEY,
-      seller: Number(env.NEXT_PUBLIC_PADDLE_SELLER_ID),
     } as unknown as InitializePaddleOptions).then(
       (paddleInstance: Paddle | undefined) => {
         if (paddleInstance) {
